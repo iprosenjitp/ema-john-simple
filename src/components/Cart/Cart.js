ca76e4b24aa2447c3f34f564e982e117993a4c55
iprosenjitp <iprosenjitp@yahoo.com>
2022-10-17 11:46:12 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({ cart, clearCart }) => {
+    // console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = parseFloat((total * 0.1).toFixed(2));
+
+    const grandTotal = parseFloat((total + shipping + tax).toFixed(2));
+
+    return (
+        <div className='cart'>
+            <h2>Order Summary</h2>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
+            <button onClick={clearCart}>Clear Cart</button>
+        </div>
+    );
+};
+
+export default Cart;
